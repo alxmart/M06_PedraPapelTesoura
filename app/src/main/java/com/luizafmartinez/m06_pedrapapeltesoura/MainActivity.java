39d5a0e0c7f14b623a8e324bc9bcd3a741f0b990
alxmart <alxmartinez@gmail.com>
2024-01-31 20:30:23 -0300

    Setting app's choice image.

--- app/src/main/java/com/luizafmartinez/m06_pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/luizafmartinez/m06_pedrapapeltesoura/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,19 +17,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void selecionarPedra(View view) {
+
         verificarGanhador("pedra");
     }
 
     public void selecionarPapel(View view) {
+
         verificarGanhador("papel");
     }
 
     public void selecionarTesoura(View view) {
+
         verificarGanhador("tesoura");
     }
 
+
+    private String gerarEscolhaAleatoriaApp() {
+
+//        Random random = new Random();
+//        random.nextInt(3);
+
+        String[] opcoes = { "pedra", "papel", "tesoura" };
+        int numeroAleatorio = new Random().nextInt(3); // 0,1,2
+
+        ImageView imagemApp = findViewById(R.id.image_app);
+
+        String escolhaApp = opcoes[numeroAleatorio];
+
+        switch ( escolhaApp ) {
+
+            case "pedra":
+                imagemApp.setImageResource( R.drawable.pedra);
+                break;
+
+            case "papel":
+                imagemApp.setImageResource( R.drawable.papel);
+                break;
+
+            case "tesoura":
+                imagemApp.setImageResource( R.drawable.tesoura);
+                break;
+        }
+
+        return escolhaApp;
+    }
+
     private void verificarGanhador( String escolhaUsuario ) {
-        System.out.println("Item clicado: " + escolhaUsuario);
+
+        String escolhaApp = gerarEscolhaAleatoriaApp();
+        //System.out.println("Item clicado: " + escolhaUsuario);
     }
 
 }
